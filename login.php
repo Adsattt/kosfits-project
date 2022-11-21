7757462581312ce371cd7b82b16cf74de489cf94
Adi Satria <98380393+Adsattt@users.noreply.github.com>
2022-11-21 17:50:10 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,82 @@
+<?php
+
+require 'function.php';
+
+if(isset($_POST["btnLogin"])){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $result = mysqli_query($koneksi, "SELECT * FROM users WHERE username = '$username'");
+
+    // cek username
+    if(mysqli_num_rows($result) === 1){
+        // cek password
+        $row = mysqli_fetch_assoc($result);
+        if(password_verify($password, $row["password"])){
+            header("Location: index.php");
+            exit;
+        }
+    }
+    $error = true;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN PAGE</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        LOGIN PAGE
+                    </div>
+                    <form action="" method="POST">
+                        <div class="card-body">
+                            <label for="username" class="form-label">Username</label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                                </svg></span>
+                                <input type="text" class="form-control" id="username" name="username" require 
+                                placeholder="Input your username" aria-describedby="basic-addon3">
+                            </div>
+
+                            <label for="password" class="form-label">Password</label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+                                </svg></span>
+                                <input type="password" class="form-control" id="password" name="password" require 
+                                placeholder="Input your password" aria-describedby="basic-addon3">
+                            </div>
+
+                            <div class="row mb-3">
+                                <button type="submit" class="btn btn-primary" name="btnLogin">Login</button>
+                            </div>
+
+                            <div class="text-center">
+                                Don't have an account? <a href="daftar.php">Sign up</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
