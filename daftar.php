a56802f02053e1435ddf4e842c9a0b6e65e5120d
Adi Satria <98380393+Adsattt@users.noreply.github.com>
2022-11-21 17:50:02 +0700

    Create daftar.php

---
+++ daftar.php
@@ -0,0 +1,78 @@
+<?php
+//  fungsi buat connect ke data base
+require 'function.php';
+
+if(isset($_POST["btnDaftar"])){
+    if(daftar($_POST) > 0){
+        echo "<script>
+                alert('user baru berhasil ditambahkan!')
+              </script>";
+    } else{
+        echo mysqli_error($koneksi);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIGN-UP PAGE</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        SIGN-UP PAGE
+                    </div>
+                    <form action="" method="POST">
+                        <div class="card-body">
+                            <label for="name" class="form-label">Name</label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                                </svg></span>
+                                <input type="text" class="form-control" id="name" name="name" require 
+                                placeholder="Input your name" aria-describedby="basic-addon3">
+                            </div>
+                            <label for="username" class="form-label">Username</label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                                </svg></span>
+                                <input type="text" class="form-control" id="username" name="username" require 
+                                placeholder="Input your username" aria-describedby="basic-addon3">
+                            </div>
+
+                            <label for="password" class="form-label">Password</label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+                                </svg></span>
+                                <input type="password" class="form-control" id="password" name="password" require 
+                                placeholder="Input your password" aria-describedby="basic-addon3">
+                            </div>
+
+                            <div class="row mb-3">
+                                <button type="submit" class="btn btn-primary" name="btnDaftar">Daftar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
