e2352ecf426ee54bed9e3be693a6592e3ac8093a
Adi Satria <98380393+Adsattt@users.noreply.github.com>
2022-11-28 11:42:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,31 @@
 <?php
 session_start();
 
-if(!isset($_SESSION["login"])){
+if (!isset($_SESSION["login"])) {
     header("Location: login.php");
     exit;
 }
 
 require 'function.php';
 
+$id = $_SESSION['user']['id_user'];
 if (isset($_POST["submit"])) {
+    global $koneksi;
+
+    $age = $_POST["age"];
+    $gender = $_POST["gender"];
     $height = $_POST["height"];
     $weight = $_POST["weight"];
 
-    if (empty($_POST['height'])) {
-        $errh = "<span style='color:#ed4337; font-size:17px; display:block'>Height is requried</span>";
-    } else {
-        $height = validate($_POST['height']);
-    }
+    $bmi = ($weight * 10000 / ($height * $height));
+    $bmipass = $bmi;
 
-    if (empty($_POST['weight'])) {
-        $errw = "<span style='color:#ed4337; font-size:17px; display:block'>Weight is requried</span>";
-    } else {
-        $weight = validate($_POST['weight']);
-    }
-
-    if (empty($_POST['height'] && $_POST['weight'])) {
-        echo "";
-    } else {
-        $bmi = ($weight * 10000/ ($height * $height));
-        $bmipass = $bmi;
-    }
+    $query = mysqli_query($koneksi, "INSERT INTO data_bmi VALUE ('$id', '$age', '$gender', '$height', '$weight', '$bmipass')");
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -44,49 +36,55 @@ if (isset($_POST["submit"])) {
     <title>BMI PAGE</title>
 </head>
 
-    <body>
-        <a href="logout.php">Logout</a>
-        <a href="inputjadwal.php">Jadwal kamu</a>
-        <h3><b>B</b>ody <b>M</b>ass <b>I</b>ndex Calculator</h3>
-        <form class="form" action="" id="form" method="POST">
-            <div class="row-one">
-                <input type="text" class="text-input" id="age" autocomplete="off" required/><p class="text">Age</p>
-                <label class="container">
-                    <input type="radio" name="female" id="f"><p class="text">Female</p>
-                    <span class="checkmark"></span>
-                </label>
-                <label class="container">
-                    <input type="radio" name="male" id="m"><p class="text">Male</p>
-                    <span class="checkmark"></span>
-                </label>
-            </div>
+<body>
+    <a href="logout.php">Logout</a>
+    <a href="inputjadwal.php">Jadwal kamu</a>
+    <h3><b>B</b>ody <b>M</b>ass <b>I</b>ndex Calculator</h3>
+    <form class="form" action="" id="form" method="POST">
+        <div class="row-one">
+            <input type="text" class="text-input" name="age" id="age" autocomplete="off" required />
+            <p class="text">Age</p>
+            <label class="container">
+                <input type="radio" name="gender" value="Female" id="f">
+                <p class="text">Female</p>
+                <span class="checkmark"></span>
+            </label>
+            <label class="container">
+                <input type="radio" name="gender" value="Male" id="m">
+                <p class="text">Male</p>
+                <span class="checkmark"></span>
+            </label>
+        </div>
 
-            <div class="row-two">
-                <input type="text" class="text-input" id="height" name="height" autocomplete="off" required><p class="text">Height (cm)</p>
-                <input type="text" class="text-input" id="weight" name="weight" autocomplete="off" required><p class="text">Weight (kg)</p>
-            </div>
-            <button type="submit" id="submit" name="submit">Submit</button>
-        </form>
-            <?php
-            error_reporting(0);
-                echo "<span class='pass'>Your BMI is : ".number_format($bmipass , 2) ."</span>";
-                if (isset($_POST['submit'])){
-                    if ($bmipass >= 13.6 && $bmipass <= 18.5) {
-                        echo "<span style='color:#00203FFF; display:block; margin-top:5px ;margin-right:50px'> Low body weight. You need to gain weight by eating moderately.</span>";
-                    } elseif ($bmipass > 18.5 && $bmipass < 24.9) {
-                        echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The standard of good health.</span>";
-                    } elseif ($bmipass > 25 && $bmipass < 29.9) {
-                        echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> Excess body weight. Exercise needs to reduce excess weight.</span>";
-                    } elseif ($bmipass > 30 && $bmipass < 34.9) {
-                        echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The first stage of obesity. It is necessary to choose food and exercise.</span>";
-                    } elseif ($bmipass > 35 && $bmipass < 39.9) {
-                        echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The second stage of obesity. Moderate diet and exercise are required.</span>";
-                    } elseif ($bmipass >= 40) {
-                        echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> Excess fat.<b style='color:#ed4337'> Fear of death</b>. Need a doctor advice.</span>";
-                    }
-                } else {
-                    echo "";
-                }
-            ?>
-    </body>     
+        <div class="row-two">
+            <input type="text" class="text-input" id="height" name="height" autocomplete="off" required>
+            <p class="text">Height (cm)</p>
+            <input type="text" class="text-input" id="weight" name="weight" autocomplete="off" required>
+            <p class="text">Weight (kg)</p>
+        </div>
+        <button type="submit" id="submit" name="submit">Submit</button>
+    </form>
+    <?php
+    error_reporting(0);
+    echo "<span class='pass'>Your BMI is : " . number_format($bmipass, 2) . "</span>";
+    if (isset($_POST['submit'])) {
+        if ($bmipass >= 13.6 && $bmipass <= 18.5) {
+            echo "<span style='color:#00203FFF; display:block; margin-top:5px ;margin-right:50px'> Low body weight. You need to gain weight by eating moderately.</span>";
+        } elseif ($bmipass > 18.5 && $bmipass < 24.9) {
+            echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The standard of good health.</span>";
+        } elseif ($bmipass > 25 && $bmipass < 29.9) {
+            echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> Excess body weight. Exercise needs to reduce excess weight.</span>";
+        } elseif ($bmipass > 30 && $bmipass < 34.9) {
+            echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The first stage of obesity. It is necessary to choose food and exercise.</span>";
+        } elseif ($bmipass > 35 && $bmipass < 39.9) {
+            echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The second stage of obesity. Moderate diet and exercise are required.</span>";
+        } elseif ($bmipass >= 40) {
+            echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> Excess fat.<b style='color:#ed4337'> Fear of death</b>. Need a doctor advice.</span>";
+        }
+    } else {
+        echo "";
+    }
+    ?>
+</body>
+
 </html>
