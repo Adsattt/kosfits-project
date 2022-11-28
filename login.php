5ae596cb9b39afe2855fbe5f5910f78b4728168b
Adi Satria <98380393+Adsattt@users.noreply.github.com>
2022-11-28 11:42:01 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,29 +1,46 @@
 <?php
+session_start();
+
+if (isset($_SESSION["login"])) {
+    header("Location: index.php");
+    exit;
+}
 
 require 'function.php';
 
-if(isset($_POST["btnLogin"])){
+if (isset($_POST["btnLogin"])) {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     $result = mysqli_query($koneksi, "SELECT * FROM users WHERE username = '$username'");
 
     // cek username
-    if(mysqli_num_rows($result) === 1){
+    if (mysqli_num_rows($result) === 1) {
         // cek password
         $row = mysqli_fetch_assoc($result);
-        if(password_verify($password, $row["password"])){
+        if (password_verify($password, $row["password"])) {
+            // set session
+            $_SESSION["user"] = $row;
+            $_SESSION["login"] = true;
+
+            // kalau username sama password bener di arahin ke indek.php
             header("Location: index.php");
             exit;
         }
     }
     $error = true;
+    if (isset($error)) {
+        echo "<script>
+                alert('username atau password salah!')
+              </script>";
+    }
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -36,6 +53,7 @@ if(isset($_POST["btnLogin"])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN PAGE</title>
 </head>
+
 <body>
     <div class="container mt-5">
         <div class="row justify-content-center">
@@ -49,25 +67,20 @@ if(isset($_POST["btnLogin"])){
                             <label for="username" class="form-label">Username</label>
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-                                </svg></span>
-                                <input type="text" class="form-control" id="username" name="username" require 
-                                placeholder="Input your username" aria-describedby="basic-addon3">
+                                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                                    </svg></span>
+                                <input type="text" class="form-control" id="username" name="username" require placeholder="Input your username" aria-describedby="basic-addon3">
                             </div>
-
                             <label for="password" class="form-label">Password</label>
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
-                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
-                                </svg></span>
-                                <input type="password" class="form-control" id="password" name="password" require 
-                                placeholder="Input your password" aria-describedby="basic-addon3">
+                                        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
+                                    </svg></span>
+                                <input type="password" class="form-control" id="password" name="password" require placeholder="Input your password" aria-describedby="basic-addon3">
                             </div>
-
                             <div class="row mb-3">
                                 <button type="submit" class="btn btn-primary" name="btnLogin">Login</button>
                             </div>
-
                             <div class="text-center">
                                 Don't have an account? <a href="daftar.php">Sign up</a>
                             </div>
@@ -76,7 +89,8 @@ if(isset($_POST["btnLogin"])){
                 </div>
             </div>
         </div>
-        
+
     </div>
 </body>
+
 </html>
