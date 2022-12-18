c1d8424babf1f2d98ab08d6d4a2b537eebf87cba
Adi Satria <98380393+Adsattt@users.noreply.github.com>
2022-12-18 23:44:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,122 +5,83 @@ if (!isset($_SESSION["login"])) {
     header("Location: login.php");
     exit;
 }
-
-require 'function.php';
-
-$id = $_SESSION['user']['id_user'];
-if (isset($_POST["submit"])) {
-    global $koneksi;
-
-    $age = $_POST["age"];
-    $height = $_POST["height"];
-    $weight = $_POST["weight"];
-
-    $bmi = ($weight * 10000 / ($height * $height));
-    $bmipass = $bmi;
-
-    $query = mysqli_query($koneksi, "INSERT INTO data_bmi VALUE ('$id', '$age', ' ', '$height', '$weight', '$bmipass')");
-}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Home - Brand</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;display=swap">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cabin:700&amp;display=swap">
-    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
-    <link rel="stylesheet" href="assets/css/untitled.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="homestyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Homepage</title>
 </head>
-
-<body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="77">
-    <nav class="navbar navbar-light navbar-expand-md fixed-top" id="mainNav" style="background: rgba(255,255,255,0.33);">
-        <div class="container"><a class="navbar-brand" href="#">kosfits</a><button data-bs-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-bs-target="#navbarResponsive" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i></button>
-            <div class="collapse navbar-collapse" id="navbarResponsive" style="padding-right: 0px;">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item nav-link"><a class="nav-link active" href="#about" style="background: rgba(255,255,255,0.3);">REGISTER</a></li>
-                    <li class="nav-item nav-link"><a class="nav-link" href="#download">LOGIN</a></li>
-                    <li class="nav-item nav-link"><i class="far fa-user" style="font-size: 37px;padding-bottom: 0px;margin-top: 0px;margin-right: 6px;"></i></li>
-                </ul>
-            </div>
+<nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand text-primary" href="#">KOSFITS</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-primary" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-primary" href="calc.php">BMI calculator</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-primary" href="schedule.php">Jadwal Olahraga</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-primary" href="meal.php">Jadwal Makan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-primary" href="logout.php">Logout</a>
+                </li>
+                <li class="nav-item dropdown">
+                </li>
+            </ul>
         </div>
-    </nav>
-    <header class="masthead" style="background-image:url('assets/img/intro-bg.jpg');">
-        <div class="intro-body">
-            <header>
-                <p>BODY MASS INDEX CALCULATOR</p>
-            </header>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <form class="form" action="" id="form" method="POST">
-                            <div class="row">
-                                <div class="col"><label class="col-form-label">age<input class="form-control" type="number" name="age" id="age" autocomplete="off" required style="border-radius: 30px;filter: brightness(70%) contrast(100%);border-style: solid;color: rgb(0,0,0);padding-left: 10px;text-align: center;"></label></div>
-                            </div>
-                            <div class="row">
-                                <div class="col"><label class="col-form-label">weight<input class="form-control" type="number" id="weight" name="weight" autocomplete="off" required style="border-radius: 30px;filter: brightness(70%) contrast(100%);border-style: solid;color: rgb(0,0,0);padding-left: 10px;text-align: center;"></label></div>
-                            </div>
-                            <div class="row">
-                                <div class="col"><label class="col-form-label">height<input class="form-control" type="number" id="height" name="height" autocomplete="off" required style="border-radius: 30px;filter: brightness(70%) contrast(100%);border-style: solid;color: rgb(0,0,0);padding-left: 10px;text-align: center;"></label></div>
-                            </div>
-                            <div style="margin-top: 10px;"><button class="btn btn-primary" type="submit" name="submit" id="submit" style="background: #dbdedd;border-color: rgba(0,0,0,0);border-radius: 30px;">submit</button></div> 
-                        </form>
-                    </div>
-                    <div class="col-md-6">
-                        <p style="font-size: 30PX;">Your BMI&nbsp; is 0.00</p>
-                        <?php
-                        error_reporting(0);
-                        echo "<span class='pass'>Your BMI is : " . number_format($bmipass, 2) . "</span>";
-                        if (isset($_POST['submit'])) {
-                            if ($bmipass >= 13.6 && $bmipass <= 18.5) {
-                                echo "<span style='color:#00203FFF; display:block; margin-top:5px ;margin-right:50px'> Low body weight. You need to gain weight by eating moderately.</span>";
-                            } elseif ($bmipass > 18.5 && $bmipass < 24.9) {
-                                echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The standard of good health.</span>";
-                            } elseif ($bmipass > 25 && $bmipass < 29.9) {
-                                echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> Excess body weight. Exercise needs to reduce excess weight.</span>";
-                            } elseif ($bmipass > 30 && $bmipass < 34.9) {
-                                echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The first stage of obesity. It is necessary to choose food and exercise.</span>";
-                            } elseif ($bmipass > 35 && $bmipass < 39.9) {
-                                echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The second stage of obesity. Moderate diet and exercise are required.</span>";
-                            } elseif ($bmipass >= 40) {
-                                echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> Excess fat.<b style='color:#ed4337'> Fear of death</b>. Need a doctor advice.</span>";
-                            }
-                        } else {
-                            echo "";
-                        }
-                        ?>
-                    </div>
+    </div>
+</nav>
+
+<body>
+    <div class="page-list-container">
+        <h2 class="page-list-title"></h2>
+        <div class="page-list-wrapper">
+            <div class="page-list">
+                <div class="page-list-item">
+                    <img class="page-list-item-img" src="asset/berat.jpg" alt="">
+                    <span class="page-list-item-title">BMI CALCULATOR</span>
+                    <p class="page-list-item-desc">Hitung BMI kamu disini</p>
+                    <a href="calc.php">
+                        <button class="page-list-item-button">Details</button>
+                    </a>
+                </div>
+                <div class="page-list-item">
+                    <img class="page-list-item-img" src="asset/workout.jpg" alt="">
+                    <span class="page-list-item-title">JADWAL OLAHRAGA</span>
+                    <p class="page-list-item-desc">Persiapkan jadwal olahraga sesuai kebutuhan dan keinginanmu</p>
+                    <a href="schedule.php">
+                        <button class="page-list-item-button">Details</button>
+                    </a>
+                </div>
+                <div class="page-list-item">
+                    <img class="page-list-item-img" src="asset/food.jpg" alt="">
+                    <span class="page-list-item-title">JADWAL MAKAN</span>
+                    <p class="page-list-item-desc">Atur pola makan sehari hari disini</p>
+                    <a href="meal.php">
+                        <button class="page-list-item-button">Details</button>
+                    </a>
                 </div>
             </div>
         </div>
-    </header>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="assets/js/grayscale.js"></script>
+    </div>
 </body>
-<?php
-    error_reporting(0);
-    echo "<span class='pass'>Your BMI is : " . number_format($bmipass, 2) . "</span>";
-    if (isset($_POST['submit'])) {
-        if ($bmipass >= 13.6 && $bmipass <= 18.5) {
-            echo "<span style='color:#00203FFF; display:block; margin-top:5px ;margin-right:50px'> Low body weight. You need to gain weight by eating moderately.</span>";
-        } elseif ($bmipass > 18.5 && $bmipass < 24.9) {
-            echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The standard of good health.</span>";
-        } elseif ($bmipass > 25 && $bmipass < 29.9) {
-            echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> Excess body weight. Exercise needs to reduce excess weight.</span>";
-        } elseif ($bmipass > 30 && $bmipass < 34.9) {
-            echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The first stage of obesity. It is necessary to choose food and exercise.</span>";
-        } elseif ($bmipass > 35 && $bmipass < 39.9) {
-            echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The second stage of obesity. Moderate diet and exercise are required.</span>";
-        } elseif ($bmipass >= 40) {
-            echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> Excess fat.<b style='color:#ed4337'> Fear of death</b>. Need a doctor advice.</span>";
-        }
-    } else {
-        echo "";
-    }
-    ?>
+
+</div>
+
 </html>
