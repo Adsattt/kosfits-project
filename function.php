643c64e11c538921fc7957c42f0af4252f8b6e6c
Adi Satria <98380393+Adsattt@users.noreply.github.com>
2022-12-18 22:46:25 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,46 +1,46 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $password ="";
-    $database = "kosfits";
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "kosfits";
 
-    // variable $koneksi buat connect ke database
-    $koneksi = mysqli_connect($server,$user,$password,$database) or die(mysqli_error($koneksi));
+// variable $koneksi buat connect ke database
+$koneksi = mysqli_connect($server, $user, $password, $database) or die(mysqli_error($koneksi));
 ?>
-<?php 
-    function daftar($data){
-        global $koneksi;
+<?php
+function daftar($data)
+{
+    global $koneksi;
 
-        $name = mysqli_real_escape_string($koneksi, $data["name"]);
-        $username = strtolower(stripslashes($data["username"]));
-        $password = mysqli_real_escape_string($koneksi, $data["password"]);
+    $name = mysqli_real_escape_string($koneksi, $data["name"]);
+    $username = strtolower(stripslashes($data["username"]));
+    $password = mysqli_real_escape_string($koneksi, $data["password"]);
 
-        // cek username udah dipake apa belum
-        $result = mysqli_query($koneksi,"SELECT username FROM users WHERE username = '$username'");
-        if(mysqli_fetch_assoc($result)){
-            echo "<script>
+    // cek username udah dipake apa belum
+    $result = mysqli_query($koneksi, "SELECT username FROM users WHERE username = '$username'");
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script>
                     alert('username sudah dipakai')
                   </script>";
-            return false;
-        }
-        // enkripsi password
-        $password = password_hash($password, PASSWORD_DEFAULT);
-
-        //tambah user ke database
-        mysqli_query($koneksi, "INSERT INTO users VALUE('', '$name', '$username', '$password')");
-
-        return mysqli_affected_rows($koneksi);
+        return false;
     }
+    // enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
 
-    $errh = $errw = $errg = "";
-    $height = $weight = "";
-    $bmipass = "";
+    //tambah user ke database
+    mysqli_query($koneksi, "INSERT INTO users VALUE('', '$name', '$username', '$password')");
 
-    function validate($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-    
-?>
+    return mysqli_affected_rows($koneksi);
+}
+
+$errh = $errw = $errg = "";
+$height = $weight = "";
+$bmipass = "";
+
+function validate($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
