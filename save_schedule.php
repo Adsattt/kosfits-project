25873d199c7573cfb6602fcf2f71f26da19db2e7
Adi Satria <98380393+Adsattt@users.noreply.github.com>
2022-12-18 22:46:45 +0700

    Create save_schedule.php

---
+++ save_schedule.php
@@ -0,0 +1,26 @@
+<?php
+require 'function.php';
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    echo "<script> alert('Error: No data to save.'); location.replace('schedule.php') </script>";
+    $koneksi->close();
+    exit;
+}
+extract($_POST);
+$allday = isset($allday);
+
+if (empty($id)) {
+    $sql = "INSERT INTO `schedule_list` (`title`,`description`,`start_datetime`,`end_datetime`) VALUES ('$title','$description','$start_datetime','$end_datetime')";
+} else {
+    $sql = "UPDATE `schedule_list` set `title` = '{$title}', `description` = '{$description}', `start_datetime` = '{$start_datetime}', `end_datetime` = '{$end_datetime}' where `id` = '{$id}'";
+}
+$save = $koneksi->query($sql);
+if ($save) {
+    echo "<script> alert('Schedule Successfully Saved.'); location.replace('schedule.php') </script>";
+} else {
+    echo "<pre>";
+    echo "An Error occured.<br>";
+    echo "Error: " . $koneksi->error . "<br>";
+    echo "SQL: " . $sql . "<br>";
+    echo "</pre>";
+}
+$koneksi->close();
